1970a2ae4898fb6e10409bececef89fdf7cd622c
SOW_ZOM <o26032002z@gmail.com>
2020-11-16 16:25:46 +0200

    start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pr4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
